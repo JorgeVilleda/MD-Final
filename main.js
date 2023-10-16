c6436678ea9b664a1c97e7a146814d9f94bcfc71
Jorge Villeda <jorgevilleda0509@outlook.es>
2023-10-16 12:34:11 -0600

    Se cambio el index y se elimino todo lo relacionado a la version 1

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 /* Funciones para el index */
+/* Muestra los cards de integrantes */
 
+const mostrarintegrantes = document.getElementById('sobrenosotros'); 
+const tarjetas = document.querySelectorAll('.cardintegrantes'); 
 
+mostrarintegrantes.addEventListener('click', function(event){ 
+    event.preventDefault(); 
 
-
-
-
-
+    tarjetas.forEach(function(tarjeta){ 
+        tarjeta.style.display = 'block';       
+    });
+        
+});
 /* Funciones para las paginas de conjuntos */ 
 
 /*Muestra alerta para recordar que se deben insertar comas en cada elemento del conjunto */ 
@@ -20,19 +26,7 @@ conjuntoAinput.addEventListener('click', function() {
 });
 
 /* Union de conjuntos */
-function mostrarconjuntoC(){ 
-    var idconjuntoC = document.getElementById('conjuntoC'); 
-    var resultado2 = document.getElementById('resultado2'); 
-    resultado2.style.display = 'none'; 
-
-    if(idconjuntoC.style.display === 'none'){ 
-        idconjuntoC.style.display = 'block'; 
-    }else{ 
-        idconjuntoC.style.display = 'none'; 
-    }
-}
-
-var conjuntoA, conjuntoB, conjuntoC; 
+var conjuntoA, conjuntoB; 
 
 function calcularUnion(){ 
     conjuntoA = document.getElementById('conjuntoA').value.split(', ').map(element => element.trim()); 
@@ -40,52 +34,9 @@ function calcularUnion(){
     var union = [...new Set([...conjuntoA, ...conjuntoB])]; 
 
     document.getElementById('resultado').textContent = 'A U B {' + union.join(',') + '}';
-
-    var idconjuntoC = document.getElementById('conjuntoC'); 
-    var resultado2 = document.getElementById('resultado2'); 
-
-    if(idconjuntoC.style.display === 'block'){ 
-        var resultado2 = document.getElementById('resultado2'); 
-        resultado2.textContent = ''; 
-        resultado2.style.display = 'block';
-
-        document.getElementById('btncalcresul').addEventListener('click', function(){
-            calcularABC();
-        });
-    }else { 
-        resultado2.style.display = 'none'; 
-    }
 }
-function calcularABC() {
-    conjuntoA = document.getElementById('conjuntoA').value.split(', ').map(element => element.trim()); 
-    conjuntoB = document.getElementById('conjuntoB').value.split(', ').map(element => element.trim());
-    conjuntoC = document.getElementById('conjuntoCinput').value.split(', ').map(element => element.trim());
-    var unionC = [...new Set([...conjuntoA, ...conjuntoB, ...conjuntoC])];
-
-    document.getElementById('resultado').style.display = 'none';
-    var resultado2 = document.getElementById('resultado2'); 
-    resultado2.textContent = 'A U B U C {' + unionC.join(', ') + '}';
-    resultado2.style.display = 'block'; 
-}
-window.addEventListener("load", function(){ 
-    document.getElementById("btnagregarc").addEventListener('click', function(){ 
-        alert('Para ver el resultado de A U B U C, tiene que volver a presionar el botón de "Realizar Union".');
-    })
-});
 /* Intersección de conjuntos */ 
-function mostrarconjuntoIC() {
-    var idconjuntoIC = document.getElementById('conjuntoIC');
-    var resultadointer2 = document.getElementById('resultadointer2');
-    resultadointer2.style.display = 'none';
-
-    if (idconjuntoIC.style.display === 'none') {
-        idconjuntoIC.style.display = 'block';
-    } else {
-        idconjuntoIC.style.display = 'none';
-    }
-}
-
-var conjuntoIA, conjuntoIB, conjuntoIC;
+var conjuntoIA, conjuntoIB;
 
 function calcularInterseccion() {
     conjuntoIA = new Set(document.getElementById('conjuntoIA').value.split(', ').map(element => element.trim()));
@@ -94,21 +45,6 @@ function calcularInterseccion() {
     var interseccionAB = new Set([...conjuntoIA].filter(x => conjuntoIB.has(x)));
 
     document.getElementById('resultadoo2').textContent = 'A ∩ B {' + [...interseccionAB].join(', ') + '}';
-
-    var idconjuntoIC = document.getElementById('conjuntoIC');
-    var resultadointer2 = document.getElementById('resultadointer2');
-
-    if (idconjuntoIC.style.display === 'block') {
-        var resultadointer2 = document.getElementById('resultadointer2');
-        resultadointer2.textContent = '';
-        resultadointer2.style.display = 'block';
-
-        document.getElementById('btncalcinter').addEventListener('click', function () {
-            calcularIABC();
-        });
-    } else {
-        resultadointer2.style.display = 'none';
-    }
 }
 /* Diferencia de conjuntos */ 
 
